5fd6d9caaf4c449f36c637fbc132f449c77102a5
Septian Solihin <septian.solihin@btpn.com>
2022-01-24 21:25:54 +0700

    initialization to connect mongoDB cloud

--- app/index.js
+++ app/index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const mongoose = require('mongoose');
+const DATABASE = "mongodb+srv://septian-user:fm6Fs7KjQ8AAGd3N@cluster0.zv72p.mongodb.net/db_septian_betest?retryWrites=true&w=majority";
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+mongoose.connect(DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() =>{
+  console.log('DB Connected')
+  app.listen(process.env.PORT || port, () => {
+    console.log(`app listening on port ${port}`)
+  })
 })
+  .catch((err) => console.log(err));

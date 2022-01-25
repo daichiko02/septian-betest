f8b281d3ff0813e3cabfc071fad9bd0ca6e61ece
Septian Solihin <septian.solihin@btpn.com>
2022-01-25 18:23:03 +0700

    refactor index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+const DATABASE = "mongodb+srv://septian-user:fm6Fs7KjQ8AAGd3N@cluster0.zv72p.mongodb.net/db_septian_betest?retryWrites=true&w=majority";
+const path = require('path');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser')
+const userController = require('./app/controllers/UserController')
+const authController = require('./app/controllers/AuthController')
+require('./app/kafka/KafkaConsumer')
+
+class Server{
+  constructor(){
+    this.initConfiguration();
+    this.initDatabase();
+  }
+
+  initConfiguration(){
+    dotenv.config();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:false}));
+    app.use(userController.routes())
+    app.use(authController.routes())
+  }
+
+  initDatabase(){
+    mongoose.connect(DATABASE, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }).then(() =>{
+      console.log('DB Connected')
+      this.initApplication();
+    })
+      .catch((err) => console.log(err));
+  }
+
+  initApplication(){
+    app.listen(process.env.PORT || port, () => {
+      console.log(`app listening on port ${port}`)
+    })
+  }
+}
+
+new Server();

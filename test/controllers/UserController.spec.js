7e70776506c3a69d49974bde4519dd8e2e552407
Septian Solihin <septian.solihin@btpn.com>
2022-01-25 22:28:38 +0700

    create unit test UserController for endpoint /user

---
+++ test/controllers/UserController.spec.js
@@ -0,0 +1,40 @@
+const request = require('supertest');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const userController = require('../../app/controllers/UserController')
+const authController = require('../../app/controllers/AuthController')
+const userService = require('../../app/services/UserService')
+const userModel = require('../../app/models/userModel')
+
+jest.mock('jsonwebtoken', () => {
+  return {
+    verify: jest.fn().mockReturnValueOnce({})
+  };
+});
+
+describe('UserController', () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(userController.routes())
+
+  describe('#getUser', () => {
+    it('should return http status 200 when getUser succeed', async(done) => {
+      const user = new userModel({
+        id: 111,
+        userName: 'septian',
+        accountNumber: 123,
+        emailAddress: 'septian@gmail.com',
+        identityNumber: 11501,
+      })
+      userService.fetchUserByIdentityNumber = jest.fn().mockResolvedValue(user)
+
+      await request(app)
+        .get('/user')
+        .set('x-access-token', 'token')
+        .query({identityNumber:2138123})
+        .expect(200)
+      done();
+    });
+  });
+});
